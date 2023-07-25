813a7bd9054f6983ba880fd0d61a81504202d7c9
yusharnadi <yust.44@gmail.com>
2023-07-25 23:47:11 +0700

    add delete

--- src/screens/LaporanDetail.js
+++ src/screens/LaporanDetail.js
@@ -1,22 +1,49 @@
-import {SafeAreaView, Image} from 'react-native';
+/* eslint-disable no-catch-shadow */
+/* eslint-disable no-shadow */
+import {SafeAreaView, Image, StyleSheet} from 'react-native';
 import React from 'react';
-import {AspectRatio, ScrollView, Text, Box} from 'native-base';
+import {
+  AspectRatio,
+  ScrollView,
+  Text,
+  Box,
+  Button,
+  Icon,
+  Modal,
+  FormControl,
+  Input,
+  Stack,
+} from 'native-base';
 import axios from 'axios';
 import {Host} from '../utils/Host';
 import GeneralError from '../components/GeneralError';
 import Loading from '../components/Loading';
+import {TrashIcon} from 'react-native-heroicons/outline';
+import Mapbox from '@rnmapbox/maps';
+import {Formik} from 'formik';
+import * as yup from 'yup';
+import Success from '../components/Success';
+import Error from '../components/Error';
 
 const LaporanDetail = ({route, navigation}) => {
-  //   const {id} = route.params;
+  const {id} = route.params;
   const [data, setData] = React.useState(null);
   const [error, setError] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [showModal, setShowModal] = React.useState(false);
+
+  const [isSuccess, setIsSuccess] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  Mapbox.setAccessToken(
+    'pk.eyJ1IjoieXVzaGFybmFkaSIsImEiOiJjbGp3YzVpeWgwb2FiM2luMXQwNjkzMW0yIn0.3CXh3GgHOJCbUhZgEyrAYA',
+  );
 
   const getData = async () => {
     try {
-      let res = await axios.get(`${Host}laporan`);
-      console.log(res.data.data[6]);
-      setData(res.data.data[6]);
+      let res = await axios.get(`${Host}laporan/${id}`);
+      console.log(res.data.data);
+      setData(res.data.data);
       setIsLoading(false);
     } catch (err) {
       if (err.response) {
@@ -45,6 +72,91 @@ const LaporanDetail = ({route, navigation}) => {
 
   if (isLoading) {
     return <Loading />;
+  }
+
+  //   VALIDATION SCHEMA YUP
+  const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+  const ValidationSchema = yup.object().shape({
+    noHp: yup
+      .string()
+      .matches(phoneRegExp, 'Nomor HP tidak valid')
+      .min(9, 'Minimal 9 Angka')
+      .required('Tidak boleh kosong'),
+  });
+
+  const submit = async values => {
+    console.log(typeof values);
+    // return;
+    setShowModal(false);
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post(
+        'https://sinarpju.digitaldev.id/api/laporan/' + data.id,
+        values,
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+
+      setIsSuccess(true);
+      setIsLoading(false);
+      console.log(response.data);
+      return;
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        setIsError({
+          status: true,
+          type: 'Error Response Validation Error',
+          message: error.response.data?.message,
+        });
+        setIsLoading(false);
+        return;
+      } else if (error.request) {
+        console.log(error.request);
+        setIsError({
+          status: true,
+          type: 'Error Request / Bad Request.',
+          message: error.request,
+        });
+        setIsLoading(false);
+        return;
+      } else {
+        console.log(`Unknown Error message: ${error.message}`);
+        setIsError({
+          status: true,
+          type: 'Unknown Error .',
+          message: error.message,
+        });
+      }
+    }
+  };
+
+  const handleSuccess = () => {
+    console.log('Success UI Rendered');
+
+    setIsSuccess(false);
+    navigation.popToTop();
+  };
+
+  if (isSuccess) {
+    return <Success handleSuccess={handleSuccess} />;
+  }
+
+  const handleError = () => {
+    console.log('Error UI Rendered');
+    setIsError(false);
+    // navigation.popToTop();
+  };
+
+  if (isError) {
+    return <Error handleError={handleError} error={isError} />;
   }
 
   return (
@@ -91,13 +203,106 @@ const LaporanDetail = ({route, navigation}) => {
           <Text px="5" py={2} bold>
             Koordinat
           </Text>
-          <Box p="5" backgroundColor="white">
-            <Text numberOfLines={3}>{data.latitude + data.longitude}</Text>
+          <Box backgroundColor="white">
+            <Mapbox.MapView
+              style={styles.map}
+              styleURL={Mapbox.StyleURL.Street}>
+              <Mapbox.Camera
+                zoomLevel={13}
+                centerCoordinate={[data.longitude, data.latitude]}
+                animationMode="flyTo"
+                animationDuration={2000}
+              />
+              <Mapbox.PointAnnotation
+                id="1"
+                title={'this is a marker view'}
+                coordinate={[data.longitude, data.latitude]}
+              />
+            </Mapbox.MapView>
+          </Box>
+          <Box p="5">
+            <Button
+              onPress={() => setShowModal(true)}
+              size="lg"
+              backgroundColor="blue.900"
+              rounded="lg"
+              leftIcon={<Icon as={TrashIcon} size="sm" />}>
+              Hapus
+            </Button>
           </Box>
         </Box>
       </ScrollView>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.Content maxWidth="400px">
+          <Modal.CloseButton />
+          <Modal.Header>Hapus Laporan</Modal.Header>
+          <Formik
+            validationSchema={ValidationSchema}
+            initialValues={{
+              noHp: '',
+            }}
+            onSubmit={values => {
+              submit(values);
+            }}>
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              isValid,
+            }) => (
+              <>
+                <Modal.Body>
+                  <Text>
+                    Mohon masukan nomor handphone di laporan yang akan dihapus.
+                  </Text>
+                  <FormControl isRequired isInvalid={'noHp' in errors}>
+                    <Stack mx="4">
+                      <FormControl.Label>No Handphone</FormControl.Label>
+                      <Input
+                        maxW="300"
+                        onBlur={handleBlur('noHp')}
+                        value={values.noHp}
+                        onChangeText={handleChange('noHp')}
+                        inputMode="tel"
+                      />
+                      <FormControl.ErrorMessage>
+                        {errors.noHp}
+                      </FormControl.ErrorMessage>
+                    </Stack>
+                  </FormControl>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button.Group space={2}>
+                    <Button
+                      variant="ghost"
+                      colorScheme="blueGray"
+                      onPress={() => {
+                        setShowModal(false);
+                      }}>
+                      Cancel
+                    </Button>
+                    <Button isLoading={!isValid} onPress={handleSubmit}>
+                      Save
+                    </Button>
+                  </Button.Group>
+                </Modal.Footer>
+              </>
+            )}
+          </Formik>
+        </Modal.Content>
+      </Modal>
     </SafeAreaView>
   );
 };
 
 export default LaporanDetail;
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    width: '100%',
+    height: 200,
+  },
+});
