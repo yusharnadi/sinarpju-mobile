dd4e1d38719271844d3330d47c7b92de5dc7b968
yusharnadi <yust.44@gmail.com>
2023-08-09 00:08:19 +0700

    create logout comp

---
+++ src/components/Logout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Logout() {
+  return (
+    <View>
+      <Text>Logout</Text>
+    </View>
+  );
+}
+
+export default Logout;
