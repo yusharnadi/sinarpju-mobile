7f8f23e5d42553da0eddbf056f324e58c3f37530
yusharnadi <yust.44@gmail.com>
2023-09-25 21:01:01 +0700

    ready to prod 2

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Box, HStack} from 'native-base';
-import Logo from '../assets/logo.svg';
 import {Image} from 'react-native';
 
 const TopBar = () => {
   return (
     <Box bgColor="blue.900" px="5" py="2.5">
       <HStack justifyContent="space-between" alignItems="center">
-        {/* <Logo /> */}
         <Image
           source={require('../assets/logo-head.png')}
           style={{height: 35, width: 111}}
